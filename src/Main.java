016a66bc0d4b94f24af72b00165b4ed3af104b96
hinata7010 <1234minseok@naver.com>
2024-02-09 13:54:21 +0900

    feat: Update comments in Main class

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import view.WmsMainMenu;
 
+// 메인 클래스는 프로그램의 진입점이며, WMS 시스템의 메인 메뉴를 표시하는 역할을 합니다.
 public class Main {
     public static void main(String[] args) {
         WmsMainMenu wmsMenu = new WmsMainMenu();
+
         wmsMenu.mainmenu();
     }
 }
