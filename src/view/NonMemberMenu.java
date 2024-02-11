e199a207d634648e191d7bba786a241552ac319f
hinata7010 <1234minseok@naver.com>
2024-02-11 17:45:36 +0900

    feat: Add NonMemberMenu class

---
+++ src/view/NonMemberMenu.java
@@ -0,0 +1,72 @@
+package view;
+
+import service.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NonMemberMenu {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static LoginService loginService = new LoginServiceImpl();
+    private static UserService userService = new UserServiceImpl();
+    private static VendorService vendorService = new VendorServiceImpl();
+    private static RetrievalService retrievalService = new RetrievalServiceImpl();
+    private static EstimateService estimateService = new EstimateServiceImpl();
+    public static void nonMemberMenu()
+    {
+        System.out.print(" /\\_/\\    /\\_/\\    /\\_/\\  \n" +
+                "( o.o )  ( o.o )  ( o.o ) \n" +
+                " > ^ <    > ^ <    > ^ <\n");
+        System.out.println("야옹 창고 시스템에 오신 것을 환영합니다. 원하시는 메뉴를 선택해주세요");
+        System.out.println("1. 회원가입 2. 로그인 3. 고객센터 4. 요금안내조회 5. 운송장 조회 6. 견적신청 7. 아이디 찾기 8. 비밀번호 찾기 9. 시스템 종료");
+
+        int menuno = 0;
+        try {
+            menuno = Integer.parseInt(br.readLine());
+        } catch (IOException e) {
+            System.out.println("잘못 입력하셨습니다.");
+        }
+        switch (menuno) {
+            case 1-> {
+                userService.addMember();
+                nonMemberMenu();
+            }
+            case 2-> {
+                loginService.login(); // 로그인 검사 로직 필요
+                MemberMenu.memberMainMenu();
+            }
+            case 3-> {
+                InqueryServiceMenu.inqueryServiceMenu();
+                nonMemberMenu();
+            }
+            case 4-> {
+                vendorService.getFeeInfo();
+                nonMemberMenu();
+            }
+            case 5->{
+                retrievalService.getWaybill();
+                nonMemberMenu();
+            }
+            case 6->{
+                estimateService.getEstimateRequest();
+                nonMemberMenu();
+            }
+            case 7->{
+                userService.findId();
+                nonMemberMenu();
+            }
+            case 8 ->{
+                userService.findPassword();
+                nonMemberMenu();
+            }
+            case 9 ->{
+                System.out.println("야옹 창고 시스템을 종료합니다.");
+            }
+            default -> {
+                System.out.println("잘못 입력하셨습니다.");
+                nonMemberMenu();
+            }
+        }
+    }
+}
