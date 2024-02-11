a5f8ab9bceb7832e28b7cabb75e813765ec78f4d
hinata7010 <1234minseok@naver.com>
2024-02-11 17:53:47 +0900

    feat: Add VendorService interface

--- src/service/VendorService.java
+++ src/service/VendorService.java
@@ -1,4 +1,27 @@
 package service;
 //거래처 관리
 public interface VendorService {
+    public void getFeeInfo();
+
+    public void addVendor();
+
+    public void getVendorAuthority();
+
+    public void approveVendorAuthority();
+
+    public void addFeeInfo();
+
+    public void modifyFeeInfo();
+
+    public void removeFeeInfo();
+
+    public void getAllContracts();
+
+    public void approveContract();
+
+    public void cancelContract();
+
+    public void getContractsByIndividual();
+
+    public void manageContractCalendar();
 }
