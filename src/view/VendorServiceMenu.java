24da7b95fea2e6d64aa127b4f8eff64e86622c75
hinata7010 <1234minseok@naver.com>
2024-02-11 17:54:42 +0900

    feat: Add VendorServiceMenu class

--- src/view/VendorServiceMenu.java
+++ src/view/VendorServiceMenu.java
@@ -1,4 +1,43 @@
 package view;
 
+
+
+import service.VendorService;
+import service.VendorServiceImpl;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class VendorServiceMenu {
+    public static void vendorServiceMenu()
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        VendorService vendorService = new VendorServiceImpl();
+        int menuno = 0;
+        System.out.println("1. 요금 안내 2. 거래처 업무 3. 계약 업무");
+        try {
+            menuno = Integer.parseInt(br.readLine());
+         } catch (IOException e) {
+            System.out.println("잘못 입력하셨습니다.");
+        }
+        switch (menuno) {
+            case 1 ->{
+                FeeInformationServiceMenu.feeInformationServiceMenu();
+            }
+            case 2 ->{
+                VendorServiceDetailMenu.vendorServiceDetailMenu();
+            }
+            case 3 ->{
+                ContractServiceMenu.contractServiceMenu();
+            }
+            case 4 ->{
+                VendorServiceMenu.vendorServiceMenu();
+            }
+            default -> {
+                System.out.println("잘못 입력하셨습니다.");
+                vendorServiceMenu();
+            }
+        }
+    }
 }
