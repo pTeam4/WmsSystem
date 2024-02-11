fd76943cb1a44fea1687ce5010486f7a845a0318
hinata7010 <1234minseok@naver.com>
2024-02-11 17:48:23 +0900

    feat: Add SafetyCheckServiceImpl java

--- src/service/SafetyCheckServiceImpl.java
+++ src/service/SafetyCheckServiceImpl.java
@@ -1,4 +1,28 @@
 package service;
 
-public class SafetyCheckServiceImpl {
+public class SafetyCheckServiceImpl implements SafetyCheckService {
+    @Override
+    public void addSafetyCheck() {
+
+    }
+
+    @Override
+    public void getSafetyCheckRecords() {
+
+    }
+
+    @Override
+    public void modifySafetyCheckRecord() {
+
+    }
+
+    @Override
+    public void removeSafetyCheckRecord() {
+
+    }
+
+    @Override
+    public void recordTakeFollowUpAction() {
+
+    }
 }
