938493eb588a5b7a3baddc112df51d8f72c0b632
hinata7010 <1234minseok@naver.com>
2024-02-11 17:50:10 +0900

    feat: Add UserService interface

--- src/service/UserService.java
+++ src/service/UserService.java
@@ -2,4 +2,33 @@ package service;
 //회원 관리
 public interface UserService {
 
+    public void addMember();
+
+    public void findId();
+
+    public void findPassword();
+
+    public void modifyMember();
+
+    public void removeMember();
+
+    public void approveAdministrator();
+
+    public void approveMember();
+
+    public void getUnregisteredMembersByVendor();
+
+    public void getMemberDetails();
+
+    public void getMembersByAuthority();
+
+    public void getMembers();
+
+    public void addMemberAuthority();
+
+    public void removeMemberAuthority();
+
+    public void modifyMemberAuthority();
+
+    public void getMemberAuthority();
 }
