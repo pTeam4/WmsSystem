49c86c3999399bd78dc1f58b5baa5f7b68e56df6
hinata7010 <1234minseok@naver.com>
2024-02-11 17:54:32 +0900

    feat: Add VendorServiceImpl class

--- src/service/VendorServiceImpl.java
+++ src/service/VendorServiceImpl.java
@@ -1,4 +1,63 @@
 package service;
 
-public class VendorServiceImpl {
+public class VendorServiceImpl implements VendorService {
+    @Override
+    public void getFeeInfo() {
+
+    }
+
+    @Override
+    public void addVendor() {
+
+    }
+
+    @Override
+    public void getVendorAuthority() {
+
+    }
+
+    @Override
+    public void approveVendorAuthority() {
+
+    }
+
+    @Override
+    public void addFeeInfo() {
+
+    }
+
+    @Override
+    public void modifyFeeInfo() {
+
+    }
+
+    @Override
+    public void removeFeeInfo() {
+
+    }
+
+    @Override
+    public void getAllContracts() {
+
+    }
+
+    @Override
+    public void approveContract() {
+
+    }
+
+    @Override
+    public void cancelContract() {
+
+    }
+
+    @Override
+    public void getContractsByIndividual() {
+
+    }
+
+    @Override
+    public void manageContractCalendar() {
+
+    }
 }
