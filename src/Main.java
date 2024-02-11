33a810d39121413a69474cf54fb882a5d7167f54
hinata7010 <1234minseok@naver.com>
2024-02-11 17:43:57 +0900

    refactor: Update main method functionality

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
-import view.WmsMainMenu;
+import view.NonMemberMenu;
 
 // 메인 클래스는 프로그램의 진입점이며, WMS 시스템의 메인 메뉴를 표시하는 역할을 합니다.
 public class Main {
     public static void main(String[] args) {
-        WmsMainMenu wmsMenu = new WmsMainMenu();
+        NonMemberMenu wmsMenu = new NonMemberMenu();
 
-        wmsMenu.mainmenu();
+        wmsMenu.nonMemberMenu();
     }
 }
