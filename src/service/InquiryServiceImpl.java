7181e963832bb8bc48b2accfe6e189fe289a33d8
hinata7010 <1234minseok@naver.com>
2024-02-11 17:40:28 +0900

    feat: Add InquiryServiceImpl class

--- src/service/InquiryServiceImpl.java
+++ src/service/InquiryServiceImpl.java
@@ -1,4 +1,43 @@
 package service;
 
-public class InquiryServiceImpl {
+public class InquiryServiceImpl implements InquiryService {
+    @Override
+    public void getNotice() {
+
+    }
+
+    @Override
+    public void addNotice() {
+
+    }
+
+    @Override
+    public void modifyNotice() {
+
+    }
+
+    @Override
+    public void removeNotice() {
+
+    }
+
+    @Override
+    public void getInquiry() {
+
+    }
+
+    @Override
+    public void addInquiry() {
+
+    }
+
+    @Override
+    public void removeInquiry() {
+
+    }
+
+    @Override
+    public void modifyInquiry() {
+
+    }
 }
