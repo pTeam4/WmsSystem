5272bb1502253baf68578fadb56235c2a2834a67
hinata7010 <1234minseok@naver.com>
2024-02-11 17:55:27 +0900

    feat: WarehouseService interface

--- src/service/WarehouseService.java
+++ src/service/WarehouseService.java
@@ -1,4 +1,24 @@
 package service;
 //창고 관리
 public interface WarehouseService {
+    public void addWarehouse();
+
+    public void getWarehouse();
+
+    public void warehouseStatusList();
+
+    public void getStock();
+
+    public void addPhysicalInventory();
+
+    public void modifyStockTaking();
+
+    public void removeStockTaking();
+
+    public void getStockTaking();
+    public void getStockByMajorCategory();
+    public void getStockByMiddleCategory();
+    public void getStockBySubcategory();
+    public void commmodityStatusList();
+    public void correspondentStatusList();
 }
