1e55f5dc5983d6aae8d076ff67fa0a7983798469
hinata7010 <1234minseok@naver.com>
2024-02-11 17:51:52 +0900

    feat: Add VehicleService interface

--- src/service/VehicleService.java
+++ src/service/VehicleService.java
@@ -1,4 +1,13 @@
 package service;
 //차량 관리
 public interface VehicleService {
+    public void addVehicle();
+
+    public void getVehicleList();
+
+    public void modifyVehicle();
+
+    public void removeVehicle();
+
+    public void getDispatchRecordsByVehicle();
 }
