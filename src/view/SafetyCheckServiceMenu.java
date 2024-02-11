3eba67bda4203db9dade4cd67be1d54fd034a23e
hinata7010 <1234minseok@naver.com>
2024-02-11 17:48:39 +0900

    feat: Add SafetyCheckServiceMenu class

--- src/view/SafetyCheckServiceMenu.java
+++ src/view/SafetyCheckServiceMenu.java
@@ -1,4 +1,52 @@
 package view;
+
+import service.SafetyCheckService;
+import service.SafetyCheckServiceImpl;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 //안전점검메뉴
 public class SafetyCheckServiceMenu {
+    public static void safetyCheckServiceMenu()
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        SafetyCheckService safetyCheckService = new SafetyCheckServiceImpl();
+        int menuno = 0;
+        System.out.println("1. 안전점검 실시내역 등록 2. 안전점검 내역 조회 3. 안전점검 내역 수정 4. 안전점검 내역 삭제 5. 후속조치 기록 6. 이전메뉴로 돌아가기");
+        try {
+            menuno = Integer.parseInt(br.readLine());
+        } catch (IOException e) {
+            System.out.println("잘못 입력하셨습니다.");
+        }
+        switch (menuno) {
+            case 1 ->{
+                safetyCheckService.addSafetyCheck();
+                safetyCheckServiceMenu();
+            }
+            case 2 ->{
+                safetyCheckService.getSafetyCheckRecords();
+                safetyCheckServiceMenu();
+            }
+            case 3 ->{
+                safetyCheckService.modifySafetyCheckRecord();
+                safetyCheckServiceMenu();
+            }
+            case 4 ->{
+                safetyCheckService.removeSafetyCheckRecord();
+                safetyCheckServiceMenu();
+            }
+            case 5 ->{
+                safetyCheckService.recordTakeFollowUpAction();
+                safetyCheckServiceMenu();
+            }
+            case 6 ->{
+                MemberMenu.memberMainMenu();
+            }
+            default -> {
+                System.out.println("잘못 입력하셨습니다.");
+            }
+        }
+    }
 }
