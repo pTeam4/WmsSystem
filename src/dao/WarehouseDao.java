1675b5a92bacda2d91cd215c94249b82bf40cb58
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-02-13 00:45:41 +0900

    feat: WarehouseDao클래스 warehouseInsert메소드 기능 구현

--- src/dao/WarehouseDao.java
+++ src/dao/WarehouseDao.java
@@ -3,8 +3,11 @@ package dao;
 import config.JdbcConnection;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 //창고
 public class WarehouseDao {
@@ -13,10 +16,34 @@ public class WarehouseDao {
 
     public WarehouseDao() {
         this.bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        this. connection = JdbcConnection.getInstance().getConnection();
+        this.connection = JdbcConnection.getInstance().getConnection();
     }
 
     private void warehouseInsert() {
+        String sql = "INSERT INTO warehouse (name, location, type)" +
+                "VALUES (?, ?, ?)";
 
+        try (
+                PreparedStatement preparedStatement = connection.prepareStatement(sql)
+        ) {
+
+            System.out.print("창고 이름을 입력하세요: ");
+            String name = bufferedReader.readLine();
+
+            System.out.print("창고 위치를 입력하세요: ");
+            String location = bufferedReader.readLine();
+
+            System.out.print("창고 종류를 입력하세요: ");
+            String type = bufferedReader.readLine();
+
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, location);
+            preparedStatement.setString(3, type);
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
