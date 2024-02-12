577653d5fd7aac9cb3af4d46c5ec4dcf6e04704a
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-02-13 00:14:20 +0900

    feat: WarehouseDao클래스에 생성자 추가

--- src/dao/WarehouseDao.java
+++ src/dao/WarehouseDao.java
@@ -3,6 +3,7 @@ package dao;
 import config.JdbcConnection;
 
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 
@@ -11,6 +12,11 @@ public class WarehouseDao {
     PreparedStatement preparedStatement;
     BufferedReader bufferedReader;
     Connection connection;
+
+    public WarehouseDao() {
+        this.bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        this. connection = JdbcConnection.getInstance().getConnection();
+    }
 
     private void warehouseInsert() {
 
