9a6bee74de89e88d6aeef333e5915535775816ec
hinata7010 <1234minseok@naver.com>
2024-02-12 16:45:40 +0900

    feat: Add JdbcConnection class

--- src/config/JdbcConnection.java
+++ src/config/JdbcConnection.java
@@ -1,4 +1,37 @@
 package config;
-//DB연결
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JdbcConnection {
+    private static JdbcConnection instance;
+    private Connection conn;
+
+    private JdbcConnection() {
+        try {
+            // JDBC Driver 등록
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // 연결하기
+            conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/wmssystem",
+                    "root",
+                    "admin1234"
+            );
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static JdbcConnection getInstance() {
+        if (instance == null) {
+            instance = new JdbcConnection();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return conn;
+    }
 }
