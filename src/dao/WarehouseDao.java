e02697ccc9a1a98dd9e8c5cfe0db63944f0776ff
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-02-13 00:13:17 +0900

    feat: WarehouseDao클래스에 필드 추가

--- src/dao/WarehouseDao.java
+++ src/dao/WarehouseDao.java
@@ -1,6 +1,17 @@
 package dao;
+
+import config.JdbcConnection;
+
+import java.io.BufferedReader;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 //창고
 public class WarehouseDao {
+    PreparedStatement preparedStatement;
+    BufferedReader bufferedReader;
+    Connection connection;
+
     private void warehouseInsert() {
 
     }
