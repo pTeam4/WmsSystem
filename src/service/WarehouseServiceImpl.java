e7970c054cb396d72491a2cd82aed3a897a8ffea
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-02-13 09:46:39 +0900

    feat: WarehouseServiceImpl클래스 addWarehouse메소드 기능 구현

--- src/service/WarehouseServiceImpl.java
+++ src/service/WarehouseServiceImpl.java
@@ -1,9 +1,29 @@
 package service;
 
+import config.GetTexts;
+import dao.WarehouseDao;
+import vo.Warehouse;
+
 public class WarehouseServiceImpl implements WarehouseService {
     @Override
     public void addWarehouse() {
+        WarehouseDao warehouseDao = new WarehouseDao();
+        Warehouse warehouse = new Warehouse();
 
+        System.out.print("창고 이름을 입력하세요: ");
+        String name = GetTexts.getInstance().readLine();
+
+        System.out.print("창고 위치를 입력하세요: ");
+        String location = GetTexts.getInstance().readLine();
+
+        System.out.print("창고 종류를 입력하세요: ");
+        String type = GetTexts.getInstance().readLine();
+
+        warehouse.setName(name);
+        warehouse.setLocation(location);
+        warehouse.setType(type);
+
+        warehouseDao.warehouseInsert(warehouse);
     }
 
     @Override
