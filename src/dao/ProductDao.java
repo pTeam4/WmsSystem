0f3d36fb09d16a6bbdb151899737ba8a8b2c0fb2
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-02-14 00:25:06 +0900

    feat: ProductDao클래스 생성자 추가

--- src/dao/ProductDao.java
+++ src/dao/ProductDao.java
@@ -1,4 +1,14 @@
 package dao;
+
+import config.JdbcConnection;
+
+import java.sql.Connection;
+
 //상품
 public class ProductDao {
+    private Connection connection;
+
+    public ProductDao() {
+        this.connection = JdbcConnection.getInstance().getConnection();
+    }
 }
