6c8286b52f6400256a24f69ed156963539cb99ab
hinata7010 <1234minseok@naver.com>
2024-02-13 10:49:35 +0900

    refactor: Add Date field

--- src/vo/Expense.java
+++ src/vo/Expense.java
@@ -1,11 +1,14 @@
 package vo;
 
 import lombok.Data;
+import java.util.Date;
+//지출
 
 @Data
 public class Expense {
     private int id;
     private int warehouseId;
     private String type;
+    private Date expenseDate;
     private int cost;
 }
