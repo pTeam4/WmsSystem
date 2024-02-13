7e27c49977fa00935baf4a4e33d44d97184b3c36
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-02-14 00:26:56 +0900

    feat: ProductDao클래스 productInsert메소드 추가

--- src/dao/ProductDao.java
+++ src/dao/ProductDao.java
@@ -1,8 +1,9 @@
 package dao;
 
 import config.JdbcConnection;
+import vo.Product;
 
-import java.sql.Connection;
+import java.sql.*;
 
 //상품
 public class ProductDao {
@@ -10,5 +11,39 @@ public class ProductDao {
 
     public ProductDao() {
         this.connection = JdbcConnection.getInstance().getConnection();
+    }
+
+    public int productInsert(Product product) {
+        String sql = "INSERT INTO product (name, brand, type, price, sale_price, quantity)" +
+                "VALUES (?, ?, ?, ?, ?, ?)";
+
+        int result = 0;
+
+        try (
+                PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)
+        ) {
+
+            preparedStatement.setString(1, product.getName());
+            preparedStatement.setString(2, product.getBrand());
+            preparedStatement.setString(3, product.getType());
+            preparedStatement.setInt(4, product.getPrice());
+            preparedStatement.setInt(5, product.getSalePrice());
+            preparedStatement.setInt(6, product.getQuantity());
+
+            int row = preparedStatement.executeUpdate();
+
+            if (row == 1) {
+                ResultSet resultSet = preparedStatement.getGeneratedKeys();
+
+                if (resultSet.next()) {
+                    result = resultSet.getInt(1);
+                }
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return result;
     }
 }
