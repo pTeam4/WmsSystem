11133256bff44460f2e5e1ee12afe3690f16c6d3
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-02-13 11:39:16 +0900

    feat: WarehouseDao클래스에 warehouseSelect메소드 추가

--- src/dao/WarehouseDao.java
+++ src/dao/WarehouseDao.java
@@ -4,7 +4,10 @@ import config.JdbcConnection;
 import vo.Warehouse;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 //창고
 public class WarehouseDao {
@@ -33,5 +36,32 @@ public class WarehouseDao {
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    public List<Warehouse> warehouseSelect() {
+        String sql = "SELECT * FROM warehouse";
+        List<Warehouse> warehouses = new ArrayList<>();
+
+        try (
+                PreparedStatement preparedStatement = connection.prepareStatement(sql);
+                ResultSet resultSet = preparedStatement.executeQuery()
+        ) {
+
+
+            while (resultSet.next()) {
+                Warehouse warehouse = new Warehouse();
+                warehouse.setId(resultSet.getInt("id"));
+                warehouse.setName(resultSet.getString("name"));
+                warehouse.setLocation(resultSet.getString("location"));
+                warehouse.setType(resultSet.getString("type"));
+
+                warehouses.add(warehouse);
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return warehouses;
     }
 }
