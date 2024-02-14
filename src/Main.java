052776b28e65a14d7c3d1d827e8b6a27cf858107
hinata7010 <1234minseok@naver.com>
2024-02-14 16:29:01 +0900

    Refactor : 메인 메뉴 수정

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,6 @@ import view.NonMemberMenu;
 // 메인 클래스는 프로그램의 진입점이며, WMS 시스템의 메인 메뉴를 표시하는 역할을 합니다.
 public class Main {
     public static void main(String[] args) {
-
-        NonMemberMenu wmsMenu = new NonMemberMenu();
-
-        MemberMenu memberMenu = new MemberMenu();
-        memberMenu.memberMainMenu();
-
+        NonMemberMenu.nonMemberMenu();
     }
 }
