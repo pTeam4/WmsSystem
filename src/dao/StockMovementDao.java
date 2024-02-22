39dba892601c72fdd039914ced1b0bf8b83f1867
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-02-22 14:57:37 +0900

    refactor: StockMovementDao클래스 getStockMovementsNow메소드 sql문 수정

--- src/dao/StockMovementDao.java
+++ src/dao/StockMovementDao.java
@@ -154,7 +154,7 @@ public class StockMovementDao {
     }
     
     public List<StockMovement> getStockMovementsNow() {
-        String SELECT_STOCK_MOVEMENT_QUERY = "SELECT id, product_Id, user_Id, status_code, request_Datetime, approved_Datetime FROM stock_movement ";
+        String SELECT_STOCK_MOVEMENT_QUERY = "SELECT * FROM stock_movement ";
 
         List<StockMovement> stockMovements = new ArrayList<>();
         try (PreparedStatement preparedStatement = connection.prepareStatement(SELECT_STOCK_MOVEMENT_QUERY))
